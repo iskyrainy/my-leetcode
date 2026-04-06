cd4ebadab6a17c8129818e8568eefd77dca03514
wangnan <wangnan@gdhvxcj.cn>
2026-04-06 22:29:28 +0800

    feat: finish 035

--- src/lc/lc_035.rs
+++ src/lc/lc_035.rs
@@ -1,5 +1,5 @@
 pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-    0
+    nums.partition_point(|&x| x < target) as _
 }
 
 #[cfg(test)]
@@ -8,14 +8,14 @@ mod test {
 
     #[test]
     fn test_search_insert_1() {
-        assert_eq!(2, search_insert(vec![1,3,5,6], 5));
+        assert_eq!(2, search_insert(vec![1, 3, 5, 6], 5));
     }
     #[test]
     fn test_search_insert_2() {
-        assert_eq!(1, search_insert(vec![1,3,5,6], 2));
+        assert_eq!(1, search_insert(vec![1, 3, 5, 6], 2));
     }
     #[test]
     fn test_search_insert_3() {
-        assert_eq!(4, search_insert(vec![1,3,5,6], 7));
+        assert_eq!(0, search_insert(vec![1, 3, 5, 6], -1));
     }
 }
