dc8d678ce0022a938a4a035a103fb2bbeda2148e
wangnan <wangnan@gdhvxcj.cn>
2026-04-21 22:16:40 +0800

    feat: finish 005

--- src/lc/lc_005.rs
+++ src/lc/lc_005.rs
@@ -1,8 +1,27 @@
 pub fn longest_palindrome(s: String) -> String {
-    // dp[0][n-1] = max(dp[1][n-2], dp[n-2][1])
-    // ...
-    // 
-    todo!()
+    if s.is_empty() {
+        return s;
+    }
+    let (mut start, mut end) = (0_usize, 0_usize);
+    let chs: Vec<char> = s.chars().collect();
+    let expand = |s: &Vec<char>, mut left: usize, mut right: usize| -> usize {
+        while left > 0 && right < s.len() - 1 && s[left] == s[right] {
+            left -= 1;
+            right += 1;
+        }
+        if s[left] == s[right] {
+            right - left + 1
+        } else {
+            right - left - 1
+        }
+    };
+    for i in 0..s.len() - 1 {
+        let len = std::cmp::max(expand(&chs, i, i), expand(&chs, i, i + 1));
+        if len > end - start {
+            (start, end) = (i - (len - 1) / 2, i + len / 2);
+        }
+    }
+    String::from(&s[start..=end])
 }
 
 #[cfg(test)]
@@ -12,7 +31,7 @@ mod test {
     #[test]
     fn test_longest_palindrome_1() {
         assert_eq!(
-            String::from("bab"),
+            String::from("aba"),
             longest_palindrome(String::from("babad"))
         );
     }
