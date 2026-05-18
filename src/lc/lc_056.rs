d60af2d38d677ca7ce7176faf3cc3156352b6c59
iskyrainy <wangnan5117@gmail.com>
2026-05-18 16:05:33 +0800

    feat: finish 056

--- src/lc/lc_056.rs
+++ src/lc/lc_056.rs
@@ -1,5 +1,17 @@
-pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    todo!()
+pub fn merge(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let mut res = vec![];
+    intervals.sort_by_key(|x| x[0]);
+    let mut curr = intervals[0].clone();
+    for interval in intervals.iter().skip(1) {
+        if interval[0] <= curr[1] {
+            curr[1] = curr[1].max(interval[1]);
+        } else {
+            res.push(curr);
+            curr = interval.clone();
+        }
+    }
+    res.push(curr);
+    res
 }
 
 #[cfg(test)]
